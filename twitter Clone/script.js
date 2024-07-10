177ff6966636d4b07e46d77f19ceac9a740b7eae
Aditya Yadav <adityayadaw42@gmail.com>
2024-07-10 18:38:31 +0530

    Added HTML CSS Journey

---
+++ twitter Clone/script.js
@@ -0,0 +1,10 @@
+// let button=document.querySelector("#post");
+// let input=document.querySelector(".inp");
+// console.log(button)
+// console.log(input)
+// input.addEventListener("click",()=>{
+//     button.style.opacity="1";
+// })
+// input.addEventListener("mouseout",()=>{
+//     button.style.opacity="0.6";
+// })
